dafec99277d773a073db2cacb80bf0793b1400bd
Jonathan S. Otalora <j.otalora@me.com>
2020-08-31 23:25:58 -0700

    update longest_alphavetical_substr to account for edge cases

--- src/problem-solving/longest_alphabetical_substr.py
+++ src/problem-solving/longest_alphabetical_substr.py
@@ -1,3 +1,5 @@
+# test input 'azcbobobegghakl'
+
 def longest_alphabetical_substr(s):
     z = 1     # fast pointer
     x = y = 0  # slow pointers
@@ -10,15 +12,17 @@ def longest_alphabetical_substr(s):
         if char_a <= char_b:  # include repeating characters
             substr = s[x:z]   # create substring from x to z
             y = y + 1         # increment the slow pointer
-            if z + 1 < len(s) and s[z + 1] < s[z]:  # calc the edge of the string
+            if z + 1 < len(s) and s[z + 1] < s[z]:
                 substr = substr + s[z]
+            elif z == len(s) - 1:
+                substr = substr + s[len(s) - 1]
         else:
-            # otherwise... we encounter an edge
+            # otherwise... we found an edge
             x = y = z  # reset slow ptrs to the the fast ptr
             longeststr = substr if len(substr) > len(
                 longeststr) else longeststr  # update longest substr
-            substr = ''  # reset substr
+            substr = ''
 
         z = z + 1  # increment the fast pointer
 
-    return longeststr
+    return substr if not x else longeststr
